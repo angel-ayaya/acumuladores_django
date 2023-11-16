56b57e377869da70a8b2cae27fa1bcbd33591418
angel-ayaya <108294233+angel-ayaya@users.noreply.github.com>
2023-11-16 12:27:01 -0700

    Update ALLOWED_HOSTS to allow all hosts

--- startingproject/settings.py
+++ startingproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+t74hrv!3t0)t)4b_!=r6!c^)!@zkcljsseh*hjq(^-zxplmwp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Media files
 MEDIA_URL = '/media/'
